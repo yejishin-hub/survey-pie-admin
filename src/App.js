763a4c019855fb96a59dca5a67bcaf463dff03f7
yejishin-hub <sinyeji958@gmail.com>
2026-02-26 22:23:13 +0900

    2026-02-26 React 15-4. Ant Design 설치

--- src/App.js
+++ src/App.js
@@ -1,19 +1,9 @@
+import ListPage from './pages/ListPage';
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ListPage />
     </div>
   );
 }
