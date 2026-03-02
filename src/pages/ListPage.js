41c9c4736a11c9d044a3f1d90b40cdd094f398a6
yejishin-hub <sinyeji958@gmail.com>
2026-03-02 20:34:32 +0900

    2026-03-02 React 16-1. useSWR을 이용하여 API 연동

--- src/pages/ListPage.js
+++ src/pages/ListPage.js
@@ -1,6 +1,16 @@
+import useSWR from 'swr';
+
 import MainLayout from '../layouts/MainLayout';
+import fetcher from '../lib/fetcher';
 
 function ListPage() {
+  const { data, error } = useSWR('/surveys', fetcher);
+  console.log('data', data);
+
+  if (error) {
+    console.log(error);
+  }
+
   return <MainLayout selectedKeys={['list']}>ListPage</MainLayout>;
 }
 
